aba89e6023cd083053d83d811ddd21159ae660ed
LJ <lucianojread@gmail.com>
2026-06-26 19:39:43 -0300

    Adição das especialidades como classes filhas de profissional

---
+++ src/Fisioterapeuta.java
@@ -0,0 +1,13 @@
+//Classe filha de Profissional, especialização de fisioterapeuta
+
+public class Fisioterapeuta extends Profissional {
+
+    public Fisioterapeuta(String nome, String cpf) {
+        super(nome, cpf);
+        setEspecialidade("Fisioterapia");
+    }
+
+    public Fisioterapeuta(String nome, String cpf, String registro, float valorConsulta) {
+        super(nome, cpf, "Fisioterapia", registro, valorConsulta);
+    }
+}
